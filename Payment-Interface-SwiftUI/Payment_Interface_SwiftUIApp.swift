73da88dc32a7bf4f206a71c2e50dfe1dc76521f4
MrSampaio <jusampa2@gmail.com>
2026-04-01 16:31:03 -0300

    Initial Commit

---
+++ Payment-Interface-SwiftUI/Payment_Interface_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Payment_Interface_SwiftUIApp.swift
+//  Payment-Interface-SwiftUI
+//
+//  Created by JULIO SAMPAIO on 01/04/26.
+//
+
+import SwiftUI
+
+@main
+struct Payment_Interface_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
